ca04ce8f5dc6db56c6289969331ff97f1e63c897
i-suck <ang7157@mydusd.org>
2026-09-08 10:23:05 -0700

    added algorithms for riddle

--- Riddle.java
+++ Riddle.java
@@ -1,6 +1,27 @@
-public class Riddle{
+public class riddle{
+    public static void calculateInt(int a){
+        int originalA = a;
+        a *= 2;
+        System.out.println(a);
+        a += 6;
+        System.out.println(a);
+        a /= 2;
+        System.out.println(a);
+        a -= originalA;
+        System.out.println(a);
+    };
+    public static void calculateDouble(double a){
+        double originalA = a;
+        a *= 2;
+        System.out.println(a);
+        a += 6;
+        System.out.println(a);
+        a /= 2;
+        System.out.println(a);
+        a -= originalA;
+        System.out.println(a);
+    };
     public static void main(String[] args){
-
         int posInt = 2;
         int negInt = -2;
         int zero = 0;
@@ -9,7 +30,5 @@ public class Riddle{
         double negDouble = -2.00;
 
         int startingNum = 47;
-
-
     }
 }
