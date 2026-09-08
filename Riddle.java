4d3948ecdb95add76454e5afaac2c3d895855314
charu-loves-sleep <charumathi.vijayabhaskar@gmail.com>
2026-09-08 10:20:53 -0700

    Add integer and double variable declarations

--- Riddle.java
+++ Riddle.java
@@ -1,5 +1,15 @@
-public class Riddle {
+public class Riddle{
     public static void main(String[] args){
+
+        int posInt = 2;
+        int negInt = -2;
+        int zero = 0;
+        int one = 1;
+        double posDouble = 2.00;
+        double negDouble = -2.00;
+
+        int startingNum = 47;
+
 
     }
 }
